b2a036cafe197078383194f80f798e5c7d8fca82
lavanya <lavanyaangadi@kpmg.com>
2021-10-29 12:02:05 +0530

    first commit

---
+++ EmployeeProject/Controllers/mysalaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeProject.Controllers
+{
+    public class mysalaryController : Controller
+    {
+        // GET: mysalary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
